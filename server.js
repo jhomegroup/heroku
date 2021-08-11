a7bd4b297238499c36fa6de82c93848e49ebfea8
jhomegroup <bunchachai1992@gmail.com>
2021-08-11 13:49:09 +0700

    update_4

--- server.js
+++ server.js
@@ -1,43 +1,33 @@
-"use strict";
-const line = require("node-line-bot-api");
 const express = require("express");
-const bodyParser = require("body-parser");
-const app = express();
+const line = require("@line/bot-sdk");
+const PORT = process.env.PORT || 3000;
+const axios = require("axios");
 
-// need raw buffer for signature validation
-app.use(
-  bodyParser.json({
-    verify(req, res, buf) {
-      req.rawBody = buf;
-    },
-  })
-);
-
-// init with auth
-line.init({
-  accessToken:
+const config = {
+  channelAccessToken:
     "YjCiTcq1Lj6MeprbL8l1Qr6I8r6TP607zjrCH4Gnfl2jTTxE6xf74imYP968csryfBwJOkk9cIQplpyrrt7CBoJU6gIkxIg4w9kZjV1ze1jybg+pHosy5I5UiIE8hGiVPsZtVv9RsIHTT3f8mkwTPwdB04t89/1O/w1cDnyilFU=",
-  // (Optional) for webhook signature validation
   channelSecret: "c128d16d64b047d5d5daceafa7de542b",
+};
+
+const app = express();
+app.post("/webhook", line.middleware(config), (req, res) => {
+  Promise.all(req.body.events.map(handleEvent)).then((result) =>
+    res.json(result)
+  );
 });
 
-app.post("/webhook/", line.validator.validateSignature(), (req, res, next) => {
-  // get content from request body
-  const promises = req.body.events.map((event) => {
-    // reply message
-    return line.client.replyMessage({
-      replyToken: event.replyToken,
-      messages: [
-        {
-          type: "text",
-          text: event.message.text,
-        },
-      ],
-    });
+const client = new line.Client(config);
+function handleEvent(event) {
+  if (event.type !== "message" || event.message.type !== "text") {
+    return Promise.resolve(null);
+  }
+
+  return client.replyMessage(event.replyToken, {
+    type: "text",
+    text: event.message.text,
   });
-  Promise.all(promises).then(() => res.json({ success: true }));
-});
+}
 
-app.listen(process.env.PORT || 3000, () => {
-  console.log("Example app listening on port 3000!");
+app.listen(PORT, () => {
+  console.log(`Serer is running. ${PORT}`);
 });
